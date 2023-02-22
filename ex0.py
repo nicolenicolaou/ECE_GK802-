36e580ea0682f86825d6d89abdf55509407fd248
nicolenicolaou <119522218+nicolenicolaou@users.noreply.github.com>
2023-02-22 15:50:33 +0200

    Update ex0.py

--- ex0.py
+++ ex0.py
@@ -1,5 +1,5 @@
+import time
 import requests  # εισαγωγή της βιβλιοθήκης
-
 
 def more(text):
     count = 0
@@ -10,10 +10,20 @@ def more(text):
             reply = input('Show more (y/n)? ')
             if reply == 'n':
                 break
-
-
-url = 'http://python.org/'  # προσδιορισμός του url
+                
+url = 'http://python.org/'
 
 with requests.get(url) as response:  # το αντικείμενο response
+    
     html = response.text
     more(html)
+    
+    #erotima a
+    hdrs = response.headers
+    for key,value in hdrs.items():
+        if key=="Server": 
+            print("{:30s} {}".format(key,value))
+
+    #erotimata b,c 
+    for key,value in enumerate(response.cookies): 
+        print("{} {}".format(key,int(value.expires - time.time())/86400))
