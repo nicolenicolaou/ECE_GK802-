fa916c12a13fa83a54d28c9d3e3d7b257a8d46fb
nicolenicolaou <119522218+nicolenicolaou@users.noreply.github.com>
2023-03-01 22:45:06 +0200

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
 #1η εργαστηριακή ασκηση 
-import requests  # εισαγωγή της βιβλιοθήκης
-import time
+# εισαγωγή απαραίτητων βιβλιοθηκών
+import requests  
+from datetime import datetime as dt
 
 def more(text):
     count = 0
@@ -19,12 +20,32 @@ with requests.get(url) as response:  # το αντικείμενο response
     html = response.text
     more(html)
     
-    #erotima a
+    #ερώτημα a
     hdrs = response.headers
     for key,value in hdrs.items():
         if key=="Server": 
             print("{:30s} {}".format(key,value))
+        else:
+            print("unable to find server.\n")
 
-    #erotimata b,c 
-    for key,value in enumerate(response.cookies): 
-        print("{} {}".format(key,int(value.expires - time.time())/86400))
+    #ερώτημα b          
+    cookies=response.cookies
+    a=[]
+    for key,value in enumerate(response.cookies):
+        a.append(key) 
+
+    if len(a)==0:
+        print("This website does not use cookies")        
+    
+    else: 
+        print("This website uses {} cookies \n".format(len(a)))
+        #ερώτημα c
+        for i in cookies:
+            print("Cookie name:{}".format(i.name))
+    
+            if(i.expires==None):
+                print("This cookie doesn't expire.\n")
+    
+            else:
+                print("This cookie expires at: {}\n".format(dt.fromtimestamp(i.expires)))
+    
