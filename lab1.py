26a420b781e63fdfc0bece240add28b43c95768b
nicolenicolaou <119522218+nicolenicolaou@users.noreply.github.com>
2023-03-01 23:21:53 +0200

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 #1η εργαστηριακή ασκηση 
-# εισαγωγή απαραίτητων βιβλιοθηκών
+#εισαγωγή απαραίτητων βιβλιοθηκών
 import requests  
 import time as t 
 
@@ -13,9 +13,9 @@ def more(text):
             if reply == 'n':
                 break
 
-url = input("give a url: ")    # προσδιορισμός του url
+url = input("give a url: ")    #προσδιορισμός του url
 
-with requests.get(url) as response:  # το αντικείμενο response
+with requests.get(url) as response:  #το αντικείμενο response
     
     html = response.text
     more(html)
@@ -34,7 +34,6 @@ with requests.get(url) as response:  # το αντικείμενο response
     
     else:
         print("This website uses {} cookies \n".format(len(cookies)))
-        #ερώτημα c  
         #ερώτημα c  
         for i in cookies:
             print("Cookie name:", i.name)
