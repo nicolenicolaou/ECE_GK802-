ff023119c5a77ae0e0586c0a966afe396f88c2b3
nicolenicolaou <119522218+nicolenicolaou@users.noreply.github.com>
2023-03-01 23:20:45 +0200

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,7 +1,7 @@
 #1η εργαστηριακή ασκηση 
-#εισαγωγή απαραίτητων βιβλιοθηκών
+# εισαγωγή απαραίτητων βιβλιοθηκών
 import requests  
-from datetime import datetime as dt
+import time as t 
 
 def more(text):
     count = 0
@@ -13,39 +13,33 @@ def more(text):
             if reply == 'n':
                 break
 
-url = input("give a url: ")    #προσδιορισμός του url
+url = input("give a url: ")    # προσδιορισμός του url
 
-with requests.get(url) as response:  #το αντικείμενο response
+with requests.get(url) as response:  # το αντικείμενο response
     
     html = response.text
     more(html)
     
     #ερώτημα a
     hdrs = response.headers
-    for key,value in hdrs.items():
-        if key=="Server": 
-            print("{:30s} {}".format(key,value))
-        else:
-            print("unable to find server.\n")
+    if "Server" in hdrs:
+        print("{:30s} {}".format("Server", hdrs["Server"]))
+    else:
+        print("unable to find server.\n")
 
-    #ερώτημα b          
-    cookies=response.cookies
-    a=[]
-    for key,value in enumerate(response.cookies):
-        a.append(key) 
-
-    if len(a)==0:
-        print("This website does not use cookies")        
+    #ερώτημα b  
+    cookies = response.cookies
+    if len(cookies) == 0:
+        print("This website does not use cookies")
     
-    else: 
-        print("This website uses {} cookies \n".format(len(a)))
-        #ερώτημα c
+    else:
+        print("This website uses {} cookies \n".format(len(cookies)))
+        #ερώτημα c  
+        #ερώτημα c  
         for i in cookies:
-            print("Cookie name:{}".format(i.name))
-    
-            if(i.expires==None):
-                print("This cookie doesn't expire.\n")
-    
+            print("Cookie name:", i.name)
+            if i.expires is None:
+                print("This cookie doesn't expire.")
             else:
-                print("This cookie expires at: {}\n".format(dt.fromtimestamp(i.expires)))
-    
+                expires_str = t.ctime(i.expires)
+                print(f"This cookie expires at: {expires_str}")
